521c2632a63a028b5bad084f22267f07fa3a2fff
mattjwilson <mattjwilson@live.com>
2015-04-16 20:07:54 -0700

    Starting the settings.

--- ZM.AzureUploader.Tests/UploadSettingsFacts.cs
+++ ZM.AzureUploader.Tests/UploadSettingsFacts.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ZM.AzureUploader
+namespace ZM.AzureUploader.Tests
 {
-    public class Class1
+    public class UploadSettingsFacts
     {
     }
 }
---
+++ ZM.AzureUploader/IUploadSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace ZM.AzureUploader
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Defines the structure required of objects representing a the set of settings needed to access and manage the azure storage and files.
+    /// </summary>
+    public interface IUploadSettings
+    {
+    }
+}
